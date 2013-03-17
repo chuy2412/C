499e496ad7a2f08658d9caa3b59247bdfbd411ef
Jesus Riverajimenez <riveraji@helper.egr.msu.edu>
2013-03-17 15:20:14 -0400

    added sources for X1 and X2

--- part1.m
+++ part1.m
@@ -1,1 +1,36 @@
 %C part 1
+clear
+
+n= 2500;
+
+A = ([1 -1; 1 2]);
+
+% sigma = [1 16];
+
+var1 =1;
+var2 =4;
+
+A(1,:);
+A(2,:);
+
+ n1 = normrnd(0,var1,n,1);
+ n2 = normrnd(0,var2,n,1);
+% % 
+%  hist(n1)
+%  pause
+%  hist(n2)
+%  pause
+% 
+X1 = double(zeros(n));
+X2 = double(zeros(n));
+
+for i=1:n
+%      X1(i) = A(1,1) *(n1(i)) + A(1,2)*(n1(i));
+%      X2(i) = A(2,1) *(n2(i)) + A(2,2)*(n2(i));
+
+   X1(i) = n1(i)*2;
+   X2(i) = n2(i)*2;
+end
+
+ plot(X1,X2,'x');
+
