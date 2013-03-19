9d75d079aef4d091cf6613960885236b4dde3bdc
Jesus Riverajimenez <riveraji@helper.egr.msu.edu>
2013-03-19 03:05:01 -0400

    Added ICA and SOM

--- part1.m
+++ part1.m
@@ -1,12 +1,22 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Assignment C part 1
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-clear
+%CSE885
+%Jesus Rivera
+%Description:
+%   This program Randomly creates 2 signals (S)
+%   with n samples (by default n=750).
+%   Source 1 has 0 mean and variance of 1
+%   Source 2 has 0 mean and variance of 4
+%   Then, it mixes the sources with a mixing matrix (A)
+%   to create the observable vector (X).
+%   Finally, it computes Y using ICA and SOM methods to demix (X)
+%   and plot the results with each method
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear        %clear previous values
 
-eta = 0.01;
-error=1e-2;
-iter=100;   %70;
-n= 750;      %50;  %number of elements
+eta = 0.2;   %Learning rate
+n= 750;      %number of elements
+train = 160; %Number of training steps
 
 %Initialize the weights with random values
 W = 0.01*rand(2,2);
@@ -50,65 +60,50 @@ title('Two-dimensional distribution of the signals X1 and X2');
 xlabel('X1');
 ylabel('X2');
 drawnow
-pause(2);
-I = double(zeros(n));
+pause(0.1);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Training Phase
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Source:
-%http://www.mathworks.com/matlabcentral/fileexchange/
-%6267-neural-networks-a-comprehensive-foundation-2e-book-companion-software/
-%content/haykin/ica.m
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-for i=1:iter
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Compute Self-Organizing Maps
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[A,W_SOM] = som(X,n, train, eta);
 
- %Demixing X with W
- y=W*X;
+W_SOM = W_SOM(1,:);
+Y_SOM = W*X;
 
- %Activation function
- f=(3/4)*y.^11+(25/4)*y.^9+(-14/3)*y.^7+(-47/4)*y.^5+(29/4)*y.^3;
- I = eye(2);
- dW=(I-f*y')*W;
+%Compute the amplitud
+Amplitude1_SOM = max(S(1,:)) / max(max(Y_SOM(1,:))); 
+Amplitude2_SOM = max(S(2,:)) / max(max(Y_SOM(2,:)));
 
- %Update the weight
- W=W+ eta*dW;
+Y_SOM(1,:) = Y_SOM(1,:) *Amplitude1_SOM;
+Y_SOM(2,:) = Y_SOM(2,:) *Amplitude2_SOM;
 
-% Break if Algorithm diverges
- if (sum(sum(isnan(W)))>0) flag=1; break; end
-   max(max(abs(dW)));
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Compute Independant Component Analysis
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[Y_ICA,W_ICA]  = ICA(X,W, n, train, eta);
 
-% Break if Algorithm converges-- max weight update is less than error
- if (max(max(abs(dW)))<error & i>10) break; end;
+%Compute the amplitude
+Amplitude1_ICA = max(S(1,:)) / max(max(Y_ICA(1,:))); 
+Amplitude2_ICA = max(S(2,:)) / max(max(Y_ICA(2,:)));
 
-end
+Y_ICA(1,:) = Y_ICA(1,:) *Amplitude1_ICA;
+Y_ICA(2,:) = Y_ICA(2,:) *Amplitude2_ICA;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Plots
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%Plot of S1
-subplot(2,2,1)
-plot(S(1,:))
-title('S1')
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %Plots the results
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+i =1:n;
+subplot(1,2,1)
+plot(i,Y_ICA(1,:),'.-',i,Y_SOM(1,:),'*-',i,S(1,:),'--')
+hleg1 = legend('Y_I_C_A_1','Y_S_O_M_1','S_2');
+%title('S1 and Y_SOM1')
 drawnow
 
 %Plot of S2
-subplot(2,2,2)
-plot(S(2,:))
-title('S2');
-drawnow
-
-%Plot of Y1
-subplot(2,2,3)
-plot(y(1,:))
-title('Y1');
-drawnow
-
-%Plot of Y2
-subplot(2,2,4)
-plot(y(2,:))
-title('Y2');
+subplot(1,2,2)
+plot(i,Y_ICA(2,:),'*-',i,Y_SOM(2,:),'.-',i,S(2,:),'--')
+hleg1 = legend('Y_I_C_A_2','Y_S_O_M_2','S_2');
+%title('S2 and Y_SOM2');
 drawnow
 
 
