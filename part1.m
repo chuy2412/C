83cd5c8dcd2d42adb736938042364a66da3fa929
Jesus Riverajimenez <riveraji@helper.egr.msu.edu>
2013-03-18 22:45:59 -0400

    Training phase tested

--- part1.m
+++ part1.m
@@ -3,13 +3,13 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear
 
-eta = 0.1;
+eta = 0.01;
 error=1e-2;
-iter=1;
-n= 10;  %number of elements
+iter=100;   %70;
+n= 750;      %50;  %number of elements
 
 %Initialize the weights with random values
-W = 0.1*rand(n,2);
+W = 0.01*rand(2,2);
 
 %Mixing matrix
 A = ([1 -1; 1 2]);
@@ -50,17 +50,12 @@ title('Two-dimensional distribution of the signals X1 and X2');
 xlabel('X1');
 ylabel('X2');
 drawnow
-
+pause(2);
 I = double(zeros(n));
 
-%%%%%%%%%%%%%%%%%%%%%%%
-%Training
-%P(x) = 1 / (sqrt(2*pi*variance)) * exp (- ((x - miu)^2)/(2*variance^2));
-
-% %Py(Y) = Px(X) / det(W);
-% Py_Y = Px(X) / det(W);
-%While Py(y) <= [||]i_to_n Py_i(Py_i)
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Training Phase
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Source:
 %http://www.mathworks.com/matlabcentral/fileexchange/
@@ -74,8 +69,8 @@ for i=1:iter
 
  %Activation function
  f=(3/4)*y.^11+(25/4)*y.^9+(-14/3)*y.^7+(-47/4)*y.^5+(29/4)*y.^3;
-
- dW=(eye(n)-f*y')*W;
+ I = eye(2);
+ dW=(I-f*y')*W;
 
  %Update the weight
  W=W+ eta*dW;
@@ -88,5 +83,34 @@ for i=1:iter
  if (max(max(abs(dW)))<error & i>10) break; end;
 
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Plots
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Plot of S1
+subplot(2,2,1)
+plot(S(1,:))
+title('S1')
+drawnow
+
+%Plot of S2
+subplot(2,2,2)
+plot(S(2,:))
+title('S2');
+drawnow
+
+%Plot of Y1
+subplot(2,2,3)
+plot(y(1,:))
+title('Y1');
+drawnow
+
+%Plot of Y2
+subplot(2,2,4)
+plot(y(2,:))
+title('Y2');
+drawnow
+
+
 
 
