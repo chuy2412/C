f1729e2be645daf12bc5012f7ee3ba7e9b38177a
Jesus Riverajimenez <riveraji@helper.egr.msu.edu>
2013-03-18 20:05:37 -0400

    Added more formulas

--- part1.m
+++ part1.m
@@ -3,7 +3,11 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear
 
+eta = 0.1;
 n= 800;  %number of elements
+
+%Initialize the weights with random values
+W = 0.1*rand(n);
 
 %Mixing matrix
 A = ([1 -1; 1 2]);
@@ -25,15 +29,15 @@ X = A*S;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Plot Histogram of S1
 subplot(3,1,1)
-hist(S(1,:))
-axis([-15 15 0 250])    %[xmin xmax ymin ymax]
+plot(S(1,:))
+%axis([-15 15 0 250])    %[xmin xmax ymin ymax]
 title('Histogram of S1')
 drawnow
 
 %Plot Histogram of S2
 subplot(3,1,2)
-hist(S(2,:))
-axis([-15 15 0 250])    %[xmin xmax ymin ymax]
+plot(S(2,:))
+%axis([-15 15 0 250])    %[xmin xmax ymin ymax]
 title('Histogram of S2');
 drawnow
 
@@ -45,6 +49,24 @@ xlabel('X1');
 ylabel('X2');
 drawnow
 
+I = double(zeros(n));
+
+%%%%%%%%%%%%%%%%%%%%%%%
+%Training
+%P(x) = 1 / (sqrt(2*pi*variance)) * exp (- ((x - miu)^2)/(2*variance^2));
+
+% %Py(Y) = Px(X) / det(W);
+% Py_Y = Px(X) / det(W);
+%While Py(y) <= [||]i_to_n Py_i(Py_i)
+
+
+% Y_transpose = X' * W';
+% Y = Y_tranpose';
+% fy = Y^3;
+% 
+% %Missing value for I
+% deltaW = eta * (I - fy*Y_transpose)* W;
+% W = W + deltaW;
 
 
 
